90d3cde59a2b76cf828d0030c6543236803185c1
nguyenphamhoangminh <nguyenphamhoangminh191204@gmail.com>
2026-09-22 18:15:43 +0700

    git commit -m "feat(service): implement updateProduct methods"

--- Slot4/product-service/product-service/src/main/java/com/fudn/product_service/service/ProductService.java
+++ Slot4/product-service/product-service/src/main/java/com/fudn/product_service/service/ProductService.java
@@ -62,10 +62,15 @@ public class ProductService {
     //   - Đừng tạo Product mới — phải update đúng record cũ để không bị tạo id khác
     // ==========================================================
     public ProductResponse updateProduct(String id, ProductRequest productRequest) {
-        // TODO: viết logic update tại đây
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new ProductNotFoundException(id));
 
-        throw new UnsupportedOperationException(
-                "TODO: Sinh vien chua implement updateProduct()");
+        product.setName(productRequest.getName());
+        product.setDescription(productRequest.getDescription());
+        product.setPrice(productRequest.getPrice());
+
+        productRepository.save(product);
+        return new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice());
     }
 
     // ==========================================================
