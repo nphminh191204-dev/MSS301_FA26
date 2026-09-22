b553fa6e31598f6bb387e7641b80a40227514d61
nguyenphamhoangminh <nguyenphamhoangminh191204@gmail.com>
2026-09-22 18:18:11 +0700

    git commit -m "feat(controller): add PUT endpoints for product"

--- Slot4/product-service/product-service/src/main/java/com/fudn/product_service/controller/ProductController.java
+++ Slot4/product-service/product-service/src/main/java/com/fudn/product_service/controller/ProductController.java
@@ -52,7 +52,11 @@ public class ProductController {
     //          @RequestBody ProductRequest productRequest) { ... }
     // ==========================================================
     // TODO: viết endpoint update tại đây
-
+    @PutMapping("/{id}")
+    public ProductResponse updateProduct(@PathVariable String id,
+                                         @RequestBody ProductRequest productRequest) {
+        return productService.updateProduct(id, productRequest);
+    }
 
     // ==========================================================
     // TODO 4 — Endpoint DELETE sản phẩm
